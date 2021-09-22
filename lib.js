55f178069f0401d4d3025a1518a0580ff65c9e7d
JoakimHa <joakimha@getacademy.no>
2021-09-22 15:01:46 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -13,7 +13,7 @@ function show() {
     <input type="text" oninput="textInput = this.value">
     <div id="fixedText">${textOutput}</div>
     <button onclick="fixtext()">fix tekst</button>
-    <button><a href src="formateringtest.html">unitTest</a></button>
+    <button><a href src="https://joakimha.github.io/uke4/formateringtest.html">unitTest</a></button>
     `
     
 }
